bf03357bf0973cd0bf9d2df63d2a88a55b3cd6b2
diegojavr <dgarcias@outlook.com>
2024-10-08 16:34:05 -0600

    Getters and setters configured to entities Compra, Cliente, Categoria

--- src/main/java/com/platzi/market/persistance/entities/Categoria.java
+++ src/main/java/com/platzi/market/persistance/entities/Categoria.java
@@ -19,4 +19,28 @@ public class Categoria {
     private String descripcion;
 
     private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- src/main/java/com/platzi/market/persistance/entities/Cliente.java
+++ src/main/java/com/platzi/market/persistance/entities/Cliente.java
@@ -23,5 +23,51 @@ public class Cliente {
     @Column(name="correo_electronico")
     private String email;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
