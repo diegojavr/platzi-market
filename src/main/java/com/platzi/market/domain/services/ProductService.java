cb439872d777d051f44e26a8b09a723c64e4d14d
diegojavr <dgarcias@outlook.com>
2024-10-11 17:34:02 -0600

    Rename

--- src/main/java/com/platzi/market/domain/services/ProductService.java
+++ src/main/java/com/platzi/market/domain/services/ProductService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class productService {
+public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
